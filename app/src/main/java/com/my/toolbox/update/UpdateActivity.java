194f18d01d8fe69c2b580ec6f4b414d5f614dd0d
balmy <rosetta1216@163.com>
2018-08-15 09:14:26 +0800

    add update

---
+++ app/src/main/java/com/my/toolbox/update/UpdateActivity.java
@@ -0,0 +1,41 @@
+package com.my.toolbox.update;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.allenliu.versionchecklib.v2.AllenVersionChecker;
+import com.allenliu.versionchecklib.v2.builder.UIData;
+import com.my.toolbox.R;
+
+/**
+ * @author balmy
+ */
+public class UpdateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                AllenVersionChecker
+                        .getInstance()
+                        .downloadOnly(UIData.create().setTitle("upgrade title")
+                                .setContent("1:111111\n2:2222222\n3333333").setDownloadUrl(""))
+                        .excuteMission(UpdateActivity.this);
+            }
+        });
+    }
+
+}
